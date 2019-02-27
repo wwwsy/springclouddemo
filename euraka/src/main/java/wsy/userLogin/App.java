e0d9251cf1561763afbfd44fbb2369a388831bfd
wwwsy <36391319+wwwsy@users.noreply.github.com>
2019-02-27 14:49:17 +0800

    新增redis共享session 登陆状态验证

--- euraka/src/main/java/wsy/userLogin/App.java
+++ euraka/src/main/java/wsy/userLogin/App.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * Hello world!
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableEurekaServer
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAspectJAutoProxy
 public class App 
 {
     public static void main( String[] args )
---
+++ euraka/src/main/java/wsy/userLogin/aop/UserLoginStateAop.java
@@ -0,0 +1,61 @@
+package wsy.userLogin.aop;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.catalina.User;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.alibaba.fastjson.JSON;
+
+import wsy.userLogin.controller.result.ResultAPI;
+import wsy.userLogin.util.JedisUtil;
+
+@Component
+@Aspect
+public class UserLoginStateAop {
+	@Autowired
+	private JedisUtil jedisUtil;
+	
+	@Pointcut("execution(public * wsy.userLogin.controller.*.*(..)) && !execution (public * wsy.userLogin.controller.LoginController.*(..))")
+	public void cut(){
+	}
+	@Around("cut()")
+	public <T> ResultAPI<?> before(ProceedingJoinPoint point) throws Throwable{
+		RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        HttpServletRequest request = sra.getRequest();      
+        String token = null;
+        Cookie[] cookies = request.getCookies();
+        for(Cookie c:cookies){
+        	if("token".equals(c.getName())){
+        		token = c.getValue();
+        	}
+        }
+		if(token==null){
+			return ResultAPI.error(401, "没有登陆");
+		}
+		token = jedisUtil.get(token);
+		if(token == null){
+			return ResultAPI.error(402, "登陆失效");
+		}
+		User user = JSON.parseObject(token, User.class);
+		if(user == null){
+			return ResultAPI.error(403, "登陆异常");
+		}
+		
+		return (ResultAPI<?>) point.proceed(point.getArgs());
+		
+		
+	}
+}
